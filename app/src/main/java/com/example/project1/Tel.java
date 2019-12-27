154f766a24820867c2a5d7b7e48e1be7f764785d
parkchaelin <pcr0827@google.com>
2019-12-27 17:15:53 +0900

    각 tab의 fragment 생성, 단일 list 구현(json X)

---
+++ app/src/main/java/com/example/project1/Tel.java
@@ -0,0 +1,31 @@
+package com.example.project1;
+
+public class Tel {
+    private String name;
+    private String tel;
+    private String birth;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+    public String getBirth() {
+        return birth;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public void setBirth(String birth) {
+        this.birth = birth;
+    }
+
+}
