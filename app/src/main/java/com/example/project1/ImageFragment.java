64b27edff445a311551e14499051ec43b2d505c3
parkchaelin <pcr0827@google.com>
2019-12-28 16:41:04 +0900

    Image tab의 이미지 크기 조정 및 이미지 크게 띄우기

--- app/src/main/java/com/example/project1/ImageFragment.java
+++ app/src/main/java/com/example/project1/ImageFragment.java
@@ -1,9 +1,11 @@
 package com.example.project1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
 import androidx.fragment.app.Fragment;
@@ -20,6 +22,20 @@ public class ImageFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_image, container, false);
         GridView gridView = (GridView) view.findViewById(R.id.gridView);
         gridView.setAdapter(new ImageAdapter(getActivity()));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Sending image id to FullScreenActivity
+                Intent i = new Intent(getActivity(), FullImageActivity.class);
+                // passing array index
+                i.putExtra("id", position);
+                startActivity(i);
+
+            }
+        });
+
+
         return view;
 
     }
